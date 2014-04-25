a9d71337a218c60a018c5cd0aa1691dc64d7fa6b
root <root@ubuntu122.04>
2014-04-25 14:23:25 +0800

    init

---
+++ config/weichat.php
@@ -0,0 +1,6 @@
+<?php
+$config['token'] = '';//填写你设定的key
+$config['appid'] = '';//填写高级调用功能的app id
+$config['appsecret'] = ''; //填写高级调用功能的密钥
+$config['debug'] = false;//是否开启调试模式
+$config['logcallback'] = true;//是否记录callback日志
---
+++ third_party/wechat_pay/tenpay_config.php
@@ -0,0 +1,18 @@
+<?php
+
+$DEBUG_ = false;
+//财付通商户号
+$PARTNER = "1900000109";
+//财付通密钥
+$PARTNER_KEY = "8934e7d15453e97507ef794cf7b0519d";
+//appid
+$APP_ID="wxd930ea5d5a258f4f";
+//appsecret
+ $APP_SECRET= "db426a9829e4b49a0dcac7b4162da6b6";	
+//paysignkey(非appkey)
+$APP_KEY="L8LrMqqeGRxST5reouB0K66CaYAWpqhAVsq7ggKkxHCOastWksvuX1uvmvQclxaHoYd3ElNBrNO2DHnnzgfVG9Qs473M3DTOZug5er46FhuGofumV8H2FVR9qkjSlC5K";
+
+//支付完成后的回调处理页面,*替换成notify_url.php所在路径
+$notify_url = "http://localhost/php/notify_url.php";
+
+?>

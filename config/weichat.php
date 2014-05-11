d0265d33662f5680cafa27fcd78885a7f4d2c29b
xianhuawei <258022429@qq.com>
2014-05-11 14:30:19 +0800

    Update weichat.php
    add weichat pay config

--- config/weichat.php
+++ config/weichat.php
@@ -4,3 +4,7 @@ $config['appid'] = '';//填写高级调用功能的app id
 $config['appsecret'] = ''; //填写高级调用功能的密钥
 $config['debug'] = false;//是否开启调试模式
 $config['logcallback'] = true;//是否记录callback日志
+
+$config['partnerid']  = ''; //财付通商户身份标识
+$config['partnerkey'] = ''; //财付通商户权限密钥Key
+$config['paysignkey'] = ''; //商户签名密钥Key
